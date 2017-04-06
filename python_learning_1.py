ef43bc2faf0324cabc1bfe6920ca6f8ab0a525bc
sriram <sriramnpkt@gmail.com>
2017-04-06 11:46:41 +0530

    add a file

---
+++ python_learning_1.py
@@ -0,0 +1,23 @@
+#Operators in Python
+a=input("Enter two numbers ")
+b=input()
+#Addition
+c=a+b
+print "The sum is ",c
+#Subtraction
+c=a-b
+print "The difference is ",c
+#Multiplication
+c=a*b
+print "The product is ",c
+#Division
+if(b==0):
+    print "!!!Enter Positive Denominator!!!"
+else:
+    c=a/b
+    print "The quotient is ",c
+    c=a%b
+    print "The remainder is ",c
+#Power Calculation
+c=a**b
+print "The a^b value is ",c
